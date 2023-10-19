a8f9a90ef6027ab6c77e43514332267e25d17a42
Lamiaa Gabriel <lamiaadev@gmail.com>
2023-10-19 19:26:01 +0300

    change products page to does exist

--- src/app/(customer)/products/page.tsx
+++ src/app/(customer)/products/page.tsx
@@ -1,24 +1,32 @@
-import type { Metadata } from "next"
+import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: `Products | ${process.env.APP_TITLE}`,
-}
-import { Products as ProductsLayout } from "@/components/products"
-import { getProducts } from "@/actions"
-import { notFound } from "next/navigation"
-import { Product } from "@prisma/client"
+};
+import { Products as ProductsLayout } from "@/components/products";
+import { getProducts } from "@/actions";
+import { notFound } from "next/navigation";
+import { Product } from "@prisma/client";
+import Empty from "@/components/empty";
 
 function sortProductsByCreatedAtDesc(products: Product[]): Product[] {
   return products.sort((a, b) => {
-    const dateA = new Date(a.created_at)
-    const dateB = new Date(b.created_at)
-    return dateB.getTime() - dateA.getTime()
-  })
+    const dateA = new Date(a.created_at);
+    const dateB = new Date(b.created_at);
+    return dateB.getTime() - dateA.getTime();
+  });
 }
 
 const Products = async () => {
-  const products = sortProductsByCreatedAtDesc(await getProducts())
+  const products = sortProductsByCreatedAtDesc(await getProducts());
 
-  if (!products.length) notFound()
+  if (!products.length) {
+    return (
+      <Empty
+        title={<div className="flex items-center gap-3">NO PRODUCTS.</div>}
+        link={{ to: "/", title: "Go Home" }}
+      />
+    );
+  }
 
   return (
     <section>
@@ -26,6 +34,6 @@ const Products = async () => {
         <ProductsLayout products={products} />
       </div>
     </section>
-  )
-}
-export default Products
+  );
+};
+export default Products;
